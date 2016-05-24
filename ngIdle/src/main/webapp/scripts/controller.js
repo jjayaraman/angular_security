d413c64fa1e7400a5f59b17cd6a0a61a8309e359
Jayakumar Jayaraman <india.jai@gmail.com>
2016-05-24 10:42:23 +0100

    updated

--- ngIdle/src/main/webapp/scripts/controller.js
+++ ngIdle/src/main/webapp/scripts/controller.js
@@ -1,53 +1,55 @@
-var idleApp = angular.module("idleApp", [ 'ngIdle' ]);
+angular.module('demo', ['ngIdle', 'ui.bootstrap'])
+    .controller('DemoCtrl', function($scope, Idle, Keepalive, $uibModal){
+      $scope.started = false;
 
+      function closeModals() {
+        if ($scope.warning) {
+          $scope.warning.close();
+          $scope.warning = null;
+        }
 
-idleApp.config(['KeepaliveProvider', 'IdleProvider', function(KeepaliveProvider, IdleProvider) {
-  IdleProvider.idle(1*60);
-  IdleProvider.timeout(1*60);
-  KeepaliveProvider.interval(1*60);
-}]);
+        if ($scope.timedout) {
+          $scope.timedout.close();
+          $scope.timedout = null;
+        }
+      }
 
+      $scope.$on('IdleStart', function() {
+        closeModals();
 
-idleApp.run([ 'Idle', function(Idle) {
-	console.log("...run....");
-	Idle.watch();
-} ]);
+        $scope.warning = $uibModal.open({
+          templateUrl: 'warning-dialog.html',
+          windowClass: 'modal-danger'
+        });
+      });
 
+      $scope.$on('IdleEnd', function() {
+        closeModals();
+      });
 
-idleApp.controller('EventsCtrl', function($scope, Idle) {
-    $scope.events = [];
+      $scope.$on('IdleTimeout', function() {
+        closeModals();
+        $scope.timedout = $uibModal.open({
+          templateUrl: 'timedout-dialog.html',
+          windowClass: 'modal-danger'
+        });
+      });
 
-    $scope.$on('IdleStart', function() {
-        // the user appears to have gone idle
-    	console.log("...IdleStart....");    	
+      $scope.start = function() {
+        closeModals();
+        Idle.watch();
+        $scope.started = true;
+      };
+
+      $scope.stop = function() {
+        closeModals();
+        Idle.unwatch();
+        $scope.started = false;
+
+      };
+    })
+    .config(function(IdleProvider, KeepaliveProvider) {
+      IdleProvider.idle(5);
+      IdleProvider.timeout(5);
+      KeepaliveProvider.interval(10);
     });
-
-    $scope.$on('IdleWarn', function(e, countdown) {
-        // follows after the IdleStart event, but includes a countdown until the user is considered timed out
-        // the countdown arg is the number of seconds remaining until then.
-        // you can change the title or display a warning dialog from here.
-        // you can let them resume their session by calling Idle.watch()
-    	
-    	console.log("...IdleWarn....");    	
-    });
-
-    $scope.$on('IdleTimeout', function() {
-        // the user has timed out (meaning idleDuration + timeout has passed without any activity)
-        // this is where you'd log them
-    	
-    	console.log("...IdleTimeout....");    	
-    });
-
-    $scope.$on('IdleEnd', function() {
-        // the user has come back from AFK and is doing stuff. if you are warning them, you can use this to hide the dialog
-    	
-    	console.log("...IdleEnd....");    	
-    });
-
-    $scope.$on('Keepalive', function() {
-        // do something to keep the user's session alive
-    	
-    	console.log("...Keepalive....");    	
-    });
-
-})
